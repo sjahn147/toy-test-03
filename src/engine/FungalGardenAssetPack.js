8c8b073edb88293b7301bec8730ef823cdaa9817
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 01:40:36 +0900

    Repair fungal asset pack contract

--- src/engine/FungalGardenAssetPack.js
+++ src/engine/FungalGardenAssetPack.js
@@ -10,6 +10,12 @@ export const FUNGAL_GARDEN_ASSET_PACK_ID = 'campaign.fungal-garden';
 export function createFungalGardenAssetPack() {
   const factory = new FungalGardenLandmarkAssetFactory();
   const bundleIds = new Set(FUNGAL_GARDEN_BUNDLE_IDS);
-
   return Object.freeze({
     id: FUNGAL_GARDEN_ASSET_PACK_ID,
+    bundleIds: FUNGAL_GARDEN_BUNDLE_IDS,
+    canCreate: bundleId => bundleIds.has(bundleId),
+    create: (bundleId, context = {}) => factory.create(bundleId, context),
+    getRecipe: getFungalGardenLandmarkRecipe,
+    listRecipes: listFungalGardenLandmarkRecipes
+  });
+}
