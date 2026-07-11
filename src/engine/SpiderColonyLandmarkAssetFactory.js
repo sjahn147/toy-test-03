b04591774f3ab744481ac627e787cfe817964b39
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 00:36:05 +0900

    Implement spider colony hero dioramas

---
+++ src/engine/SpiderColonyLandmarkAssetFactory.js
@@ -0,0 +1,4 @@
+import { THREE } from './ThreeScene.js';
+import { getSpiderColonyLandmarkRecipe } from './SpiderColonyLandmarkRecipes.js';
+
+const C = { stone:0x272633, edge:0x514c5e, silk:0xd8d3cc, shadow:0x8c8695, burned:0x353039, chitin:0x3b1d35, chitin2:0x7d315e, venom:0x77dca2, ember:0xff8d46, fire:0xffc46b, rope:0x765943, wood:0x674937, iron:0x58545d, royal:0xb1374d, gold:0xd8b15e, egg:0xe6ddd1, vein:0xb7678c, blood:0x7a2335,
