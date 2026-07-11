a7dee82dc01c136e3ce2001f7b077367642a931c
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 00:20:02 +0900

    Expose fungal garden asset pack adapter

---
+++ src/engine/FungalGardenAssetPack.js
@@ -0,0 +1,15 @@
+import { FungalGardenLandmarkAssetFactory } from './FungalGardenLandmarkAssetFactory.js';
+import {
+  FUNGAL_GARDEN_BUNDLE_IDS,
+  getFungalGardenLandmarkRecipe,
+  listFungalGardenLandmarkRecipes
+} from './FungalGardenLandmarkRecipes.js';
+
+export const FUNGAL_GARDEN_ASSET_PACK_ID = 'campaign.fungal-garden';
+
+export function createFungalGardenAssetPack() {
+  const factory = new FungalGardenLandmarkAssetFactory();
+  const bundleIds = new Set(FUNGAL_GARDEN_BUNDLE_IDS);
+
+  return Object.freeze({
+    id: FUNGAL_GARDEN_ASSET_PACK_ID,
