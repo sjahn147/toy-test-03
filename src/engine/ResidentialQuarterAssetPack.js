8825b4a97a310f6f6284b9f1de8906483b129599
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 17:14:42 +0900

    Restore complete Residential Quarter asset pack

--- src/engine/ResidentialQuarterAssetPack.js
+++ src/engine/ResidentialQuarterAssetPack.js
@@ -6,4 +6,29 @@ import {
 } from './ResidentialQuarterLandmarkRecipes.js';
 
 export class ResidentialQuarterAssetPack {
- 
+  constructor() {
+    this.factory = new ResidentialQuarterLandmarkAssetFactory();
+    this.animator = new ResidentialQuarterAssetAnimator();
+    this.id = 'campaign.residential-quarter';
+  }
+
+  canCreate(bundleId) {
+    return this.factory.canCreate(bundleId);
+  }
+
+  create(bundleId, context = {}) {
+    return this.animator.prepare(this.factory.create(bundleId, context));
+  }
+
+  update(root, elapsedSeconds) {
+    this.animator.update(root, elapsedSeconds);
+  }
+
+  getRecipe(bundleId) {
+    return getResidentialQuarterLandmarkRecipe(bundleId);
+  }
+
+  listRecipes() {
+    return listResidentialQuarterLandmarkRecipes();
+  }
+}
