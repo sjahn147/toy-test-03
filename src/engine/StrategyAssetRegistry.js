245426897ad184a824c1ade6c589f7fb86bee513
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 17:32:29 +0900

    Add stable strategy asset registry

---
+++ src/engine/StrategyAssetRegistry.js
@@ -0,0 +1,23 @@
+import { AssetRegistryPhase8 } from './AssetRegistryPhase8.js';
+import { ForwardOutpostAssetFactory } from './ForwardOutpostAssetFactory.js';
+import { isForwardOutpostProp } from '../domain/OutpostProfiles.js';
+
+export class StrategyAssetRegistry extends AssetRegistryPhase8 {
+  constructor() {
+    super();
+    this.forwardOutposts = new ForwardOutpostAssetFactory();
+  }
+
+  makeProp(prop) {
+    if (isForwardOutpost(prop)) return this.forwardOutposts.create(prop);
+    return super.makeProp(prop);
+  }
+
+  animateForwardOutpost(root, prop, time) {
+    this.forwardOutposts.animate(root, prop, time);
+  }
+}
+
+export function isForwardOutpost(prop) {
+  return isForwardOutpostProp(prop);
+}
