fa41d215eb258819532accc2b909cbd671eee5ee
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 18:15:44 +0900

    Compose worksite assets

--- src/engine/StrategyAssetRegistry.js
+++ src/engine/StrategyAssetRegistry.js
@@ -1,11 +1,13 @@
 import { AssetRegistryPhase8 } from './AssetRegistryPhase8.js';
 import { ForwardOutpostAssetFactory } from './ForwardOutpostAssetFactory.js';
+import { WorksiteAssetFactory } from './WorksiteAssetFactory.js';
 import { isForwardOutpostProp } from '../domain/OutpostProfiles.js';
 
 export class StrategyAssetRegistry extends AssetRegistryPhase8 {
   constructor() {
     super();
     this.forwardOutposts = new ForwardOutpostAssetFactory();
+    this.worksites = new WorksiteAssetFactory();
   }
 
   makeProp(prop) {
@@ -15,6 +17,14 @@ export class StrategyAssetRegistry extends AssetRegistryPhase8 {
 
   animateForwardOutpost(root, prop, time) {
     this.forwardOutposts.animate(root, prop, time);
+  }
+
+  makeWorksiteScaffold(prop) {
+    return this.worksites.createScaffold(prop);
+  }
+
+  animateWorksiteScaffold(root, prop, time) {
+    this.worksites.animateScaffold(root, prop, time);
   }
 }
 
