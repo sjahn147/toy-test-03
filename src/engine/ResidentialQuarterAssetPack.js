8292c7dbd288c7c25849acf0e125627153dc1b23
Ahn Seongjun <evdea147@sogang.ac.kr>
2026-07-12 17:05:29 +0900

    Polish Residential Quarter pack contract

--- src/engine/ResidentialQuarterAssetPack.js
+++ src/engine/ResidentialQuarterAssetPack.js
@@ -1,12 +1,9 @@
 import { ResidentialQuarterLandmarkAssetFactory } from './ResidentialQuarterLandmarkAssetFactory.js';
 import { ResidentialQuarterAssetAnimator } from './ResidentialQuarterAssetAnimator.js';
-import { getResidentialQuarterLandmarkRecipe, listResidentialQuarterLandmarkRecipes } from './ResidentialQuarterLandmarkRecipes.js';
+import {
+  getResidentialQuarterLandmarkRecipe,
+  listResidentialQuarterLandmarkRecipes
+} from './ResidentialQuarterLandmarkRecipes.js';
 
-export class ResidentialQuarterAssetPack{
-  constructor(){ this.factory=new ResidentialQuarterLandmarkAssetFactory(); this.animator=new ResidentialQuarterAssetAnimator(); this.id='campaign.residential-quarter'; }
-  canCreate(bundleId){ return this.factory.canCreate(bundleId); }
-  create(bundleId,context={}){ return this.animator.prepare(this.factory.create(bundleId,context)); }
-  update(root,elapsedSeconds){ this.animator.update(root,elapsedSeconds); }
-  getRecipe(bundleId){ return getResidentialQuarterLandmarkRecipe(bundleId); }
-  listRecipes(){ return listResidentialQuarterLandmarkRecipes(); }
-}
+export class ResidentialQuarterAssetPack {
+ 
